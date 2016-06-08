32a3e4bbc4ef2f9e5eedff29a8c8ef8e1e8bb45b
aloha <ajp529@gmail.com>
2016-06-07 21:08:09 -0600

    7716

--- main.js
+++ main.js
@@ -1,7 +1,42 @@
-$(document).ready(function(){
-$('#b1').click(
-    function(){
-        $('#output').val($('#output').val()+$(this).val());
-    }
-);
+//this function needs to take whatever id is passed to it
+$(function addNum(){
+	$(".calcButton").click(function() {
+		document.getElementById('output').innerHTML+=$(this).html();	
+	});
 });
+
+$(function addOp(){
+	$(".calcButtonOp").click(function() {
+		document.getElementById('output').innerHTML+=this.value;
+	});
+});
+
+$(function clear(){
+	$("#clear").click(function() {
+		document.getElementById('output').innerHTML=null;
+	});
+});
+
+function evaluate(){
+	eval($("#output").innerHTML)
+	//grab innerhtml of #output and eval it when user hits equals
+};
+
+
+//If no numbers added do not allow operands to be added, IF number added, 
+// allow operands, IF operand added, do not allow another operand to be added
+/*
+function allowOp(){
+	if (no numbers have been added to output box){
+		do not allow operands to be added
+	} else if (numbers have been added to output box) {
+		allow operand to be added
+	} else if (if one operand has been added){
+		do not allow any more operands to be added
+	} else {
+		console.log(the error)
+	}
+
+};
+*/
+
